37ce20d045e591d08f01483225563732902aa1fa
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-30 15:54:51 -0300

    feat: create new service create
    service to create a new order and update the products

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,17 +1,31 @@
 import { Order } from '../interface/order.interface';
 import connection from '../models/connection';
 import OrderModel from '../models/order.model';
+import ProductModel from '../models/product.model';
 
 class OrderService {
   model: OrderModel;
 
+  product: ProductModel;
+
   constructor() {
     this.model = new OrderModel(connection);
+    this.product = new ProductModel(connection);
   }
 
   async getAll(): Promise<Order[]> {
     const orders = await this.model.getAll();
     return orders;
+  }
+
+  async create(id: number, products: number[]): Promise<void> {
+    const { id: orderId } = await this.model.create(id);
+    await Promise.all(products.map(
+      (productId: number): number => {
+        this.product.update(productId, orderId);
+        return productId;
+      },
+    ));
   }
 }
 
