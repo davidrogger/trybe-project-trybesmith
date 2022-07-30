96f1792c80f7d833a640ca1f20ccf31bd01d587c
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-30 15:57:15 -0300

    feat: create new controller method
    to create a new order and update the products

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from 'express';
 import OrderService from '../services/order.service';
+import Validate from '../services/validation.service';
 
 class OrderController {
-  constructor(private orderService = new OrderService()) {}
+  validate: Validate;
+
+  constructor(private orderService = new OrderService()) {
+    this.validate = new Validate();
+  }
 
   getAll = async (_req: Request, res: Response): Promise<void> => {
     const orders = await this.orderService.getAll();
     res.status(200).json(orders);
+  };
+
+  create = async (req: Request, res: Response) => {
+    const { id: userId } = res.locals.user;
+    const { productsIds } = this.validate.productOrderBody(req.body);
+    await this.orderService.create(userId, productsIds);
+    res.status(201).json({ userId, productsIds });
   };
 }
 
--- src/interface/order.interface.ts
+++ src/interface/order.interface.ts
@@ -3,7 +3,9 @@ export type Index = {
 };
 
 export type Order = Index & {
-  id: number;
+  id?: number;
   userId: number;
   productsIds: number[];
 };
+
+export type ProductOrder = Omit<Order, 'id' | 'userId'>;
