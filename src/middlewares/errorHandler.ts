c78fe33eb52133a4acefaefcb1bf46d3434b9e7c
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-30 11:48:34 -0300

    feat: create new status
    status to unauthorized access

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 const statusLibrary: Record<string, number> = {
   ValidationError: 400,
+  Unauthorized: 401,
   Unprocessable: 422,
 };
 
